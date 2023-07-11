e2dc826eded9685a5432b35c4ff5e551f2454e94
sakshamsaxena22 <38011670+sakshamsaxena22@users.noreply.github.com>
2023-07-12 05:11:13 +0530

    Add files via upload

---
+++ sumofthreenumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class sumofthreenumbers{
+	public static void main(String args[]){
+		int num1,num2,num3,num4,num5,result;
+		System.out.println("Enter any 5 integers ");
+		Scanner in = new Scanner(System.in);
+		num1 = in.nextInt();
+		num2 = in.nextInt();
+		num3 = in.nextInt();
+		num4 = in.nextInt();
+		num5 = in.nextInt();
+	    result=num1+num2+num3+num4+num5;
+		System.out.println("The result after addition is "+result);
+	}
+}
